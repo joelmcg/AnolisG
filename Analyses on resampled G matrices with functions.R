d7afc167151265e33d8f577050ac5cc7f4673546
Joel McGlothlin <45607957+joelmcg@users.noreply.github.com>
2021-09-25 14:12:08 -0400

    Revision 9-25-2021

--- Analyses on resampled G matrices with functions.R
+++ Analyses on resampled G matrices with functions.R
@@ -4,29 +4,36 @@
 
 library(evolqg)
 library(mgcv)
-library(ppcor)
+library(geomorph)
 library(ape)
 library(phytools)
 library(tidyverse)
 
-#Summary functions
+# Species names
+species <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", 
+             "sagrei", "smaragdinus")
+species6 <- c("cristatellus", "evermanni", "grahami", "lineatopus", "sagrei", 
+              "smaragdinus")
+spp <- c("cris", "ever", "grah", "line", "pulc", "sagr", "smar")
+
+# Summary functions
 
 summarize_cols <- function (x) {
-  estimate <- apply(simplify2array(x), 2, mean)
+  estimate <- apply(simplify2array(x), 2, median)
   ci.025 <- apply(simplify2array(x), 2, quantile, probs = 0.025)
   ci.975 <- apply(simplify2array(x), 2, quantile, probs = 0.975)
   data.frame(estimate, ci.025, ci.975)
 }
 
 summarize_reps <- function (x) {
-  estimate <- apply(simplify2array(x), 1, mean)
+  estimate <- apply(simplify2array(x), 1, median)
   ci.025 <- apply(simplify2array(x), 1, quantile, probs = 0.025)
   ci.975 <- apply(simplify2array(x), 1, quantile, probs = 0.975)
   data.frame(estimate, ci.025, ci.975)
 }
 
 summarize_vec <- function(x) {
-  estimate <- mean(x)
+  estimate <- median(x)
   ci.025 <- quantile(x, probs = 0.025)
   ci.975 <- quantile(x, probs = 0.975)
   df <- data.frame(estimate, ci.025, ci.975)
@@ -35,21 +42,17 @@ summarize_vec <- function(x) {
 }
 
 
-
-
 # Perform random skewers for all species on each set of replicates
 
 rslist <- lapply(Gset, RandomSkewers, numvectors = 10000)
 rslist <- transpose(rslist)$correlations
 
 
-# Report mean and 95% confidence intervals across replicates for random skewers correlations. Shows zeroes on
-# diagonals and in upper triangle.
+# Report mean and 95% confidence intervals across replicates for random skewers 
+# correlations. Shows zeroes on diagonals and in upper triangle. (Table A2)
 
-spp <- c("cris", "ever", "grah", "line", "pulc", "sagr", "smar")
-
-rslistmean <- apply(simplify2array(rslist), 1:2, mean)
-rownames(rslistmean) <- colnames(rslistmean) <- spp
+rslistmedian <- apply(simplify2array(rslist), 1:2, median)
+rownames(rslistmedian) <- colnames(rslistmedian) <- spp
 
 rslist025 <- apply(simplify2array(rslist), 1:2, quantile, probs = 0.025)
 rownames(rslist025) <- colnames(rslist025) <- spp
@@ -58,57 +61,58 @@ rslist975 <- apply(simplify2array(rslist), 1:2, quantile, probs = 0.975)
 rownames(rslist975) <- colnames(rslist975) <- spp
 
 
-# Correlations between genetic distance vector and random skewers correlation vector for each random sample.
-# Doesn't perform Mantel test permuation. Report mean and 95% confidence intervals.
-
-geneticdist <- c(0.64127367, 0.987323604, 0.987323604, 0.541900856, 0.987323604, 1, 0.987323604, 0.987323604, 0.64127367, 
-    0.987323604, 1, 0.744421662, 0.987323604, 0.894946327, 1, 0.987323604, 0.894946327, 1, 0.987323604, 1, 1)
-
-corlist <- c()
-for (i in 1:10000) {
-    skew1 <- rslist[[i]][lower.tri(rslist[[i]], diag = FALSE)]
-    corlist[i] <- cor(geneticdist, skew1)
-}
-rm(skew1)
-
-corsummary <- summarize_vec(corlist)
-
-
-
-# Partial correlations between ecomorph identity and random skewers, controlling for genetic distance
-
-geneticdist6 <- c(0.64127367, 0.987323604, 0.987323604, 0.987323604, 1, 0.987323604, 0.987323604, 0.987323604, 1, 0.744421662, 
-    0.89496327, 1, 0.987323604, 1, 1)
-
-eco.mat <- c(0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0)
-
-corlist2 <- c()
-
-for (i in 1:10000) {
-    rs6 <- rslist[[i]][c(1:4, 6:7), c(1:4, 6:7)]
-    skew1 <- rs6[lower.tri(rs6, diag = FALSE)]
-    pcordat <- as.data.frame(cbind(eco.mat, geneticdist6, skew1))
-    corlist2[i] <- pcor(pcordat)$estimate[1, 3]
-}
-rm(rs6, pcordat, skew1)
-
-cor2summary <- summarize_vec(corlist2)
-
-
-
-
-# Read trees and data for phylogenetic analyses. Trees as newick format, data as csv with one line per species,
-# listing element and SE (from ASReml) separately
+# Read trees and data for phylogenetic analyses. Trees as newick format, data as 
+# csv with one line per species, listing element and SE (from ASReml) separately
 
 rge <- as.data.frame(read.csv("data/rge.csv"))
 
 a6tree <- read.tree("data/pruned3.tre")
 anoletree <- read.tree("data/pruned7.tre")
 anole.data <- as.data.frame(read.table("data/anoldat.txt"))
-
 eco <- as.integer(anole.data$ecomorph)
 names(eco) <- rownames(anole.data)
 
+# K_mult for rRS (text)
+
+rsmatpcoa <- function(x, sp.names){
+  skmat <-  matrix(rep(1,length(x)), nrow(x), ncol(x))
+  skmat[lower.tri(skmat, diag = FALSE)] <- x[lower.tri(x, diag=FALSE)]
+  skmat[upper.tri(skmat, diag = FALSE)] <- t(skmat)[upper.tri(skmat, 
+                                                              diag = FALSE)]
+  row.names(skmat) <- sp.names
+  colnames(skmat) <- sp.names
+  skdist <- sqrt(2*(1-skmat))
+  pcoa(skdist)$vectors
+}
+
+skewdist <- lapply(rslist, rsmatpcoa, species)
+
+Kmult <- function(x, phy){
+  physignal(x, phy, iter = 2)$phy.signal
+}
+
+Kmults <- sapply(skewdist, Kmult, anoletree)
+
+summarize_vec(Kmults)
+
+# Correlations between rRS and ecomorph (text)
+
+eco.mat <- c(0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0)
+eco6mat <- matrix(rep(0,36), 6, 6)
+eco6mat[lower.tri(eco6mat, diag = FALSE)] <- 1 - eco.mat
+eco6mat[upper.tri(eco6mat, diag = FALSE)] <- t(eco6mat)[upper.tri(eco6mat, 
+                                                                  diag = FALSE)]
+colnames(eco6mat) <- species6
+row.names(eco6mat) <- species6
+
+ecocors <- c()
+for (i in 1:10000) {
+  rs6 <- rslist[[i]][c(1:4, 6:7), c(1:4, 6:7)]
+  skew1 <- sqrt(2*(1-as.dist(rs6)))
+  ecocors[i] <- cor(skew1, as.dist(eco6mat))
+}
+
+summarize_vec(ecocors)
 
 
 # Construct phylogenetic covariance matrix
@@ -116,18 +120,64 @@ names(eco) <- rownames(anole.data)
 V <- corMatrix(Initialize(corBrownian(phy = a6tree), anole.data))
 invV <- solve(V)
 
+# Resampled phylogenetic signal (table A3)
+
+Kset <- list()
+for (i in 1:10000) {
+  Kset2 <- c()
+  for (j in 1:28) {
+    rvec <- c(rnorm(1, rge[j, 2], rge[j, 9]), rnorm(1, rge[j, 3], rge[j, 10]), 
+              rnorm(1, rge[j, 4], rge[j, 11]), rnorm(1, rge[j, 5], rge[j, 12]), 
+              rnorm(1, rge[j, 6], rge[j, 13]), rnorm(1, rge[j, 7], rge[j, 14]), 
+              rnorm(1, rge[j, 8], rge[j, 15]))
+    names(rvec) <- species
+    Kset2[j] <- phylosig(anoletree, rvec)
+  }
+  Kset[[i]] <- Kset2
+}
+rm(rvec, Kset2)
+
+Ksummary <- summarize_reps(Kset)
+
+K.mat <- matrix(NA,8,8) 
+K.mat[upper.tri(K.mat)] <- Ksummary[,1]
+K.mat[lower.tri(K.mat)] <- t(K.mat)[lower.tri(t(K.mat))]
+
+
+Kvarset <- list()
+for (i in 1:10000) {
+  Kset2 <- c ()
+  for (j in 1:8) {
+    vvec <- c(mycrisG[[i]][j,j], myeverG[[i]][j,j], mygrahG[[i]][j,j], 
+              mylineG[[i]][j,j], mypulcG[[i]][j,j], mysagrG[[i]][j,j], 
+              mysmarG[[i]][j,j])
+    names(vvec) <- species
+    Kset2[j] <- phylosig(anoletree, vvec)
+  }
+  Kvarset[[i]] <- Kset2
+}
+rm(vvec, Kset2)
+
+Kvarsummary <- summarize_reps(Kvarset)
+
+
+
 # Resample evolutionary correlation between ecomorph and genetic correlations
+# (table A4)
 
 rset <- c()
 for (i in 1:10000) {
     rset2 <- c()
     for (j in 1:28) {
-        rvec <- c(rnorm(1, rge[j, 2], rge[j, 9]), rnorm(1, rge[j, 3], rge[j, 10]), rnorm(1, rge[j, 4], rge[j, 11]), 
-            rnorm(1, rge[j, 5], rge[j, 12]), rnorm(1, rge[j, 7], rge[j, 14]), rnorm(1, rge[j, 8], rge[j, 15]))
-        names(rvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "sagrei", "smaragdinus")
+        rvec <- c(rnorm(1, rge[j, 2], rge[j, 9]), rnorm(1, rge[j, 3], 
+                  rge[j, 10]), rnorm(1, rge[j, 4], rge[j, 11]), 
+                  rnorm(1, rge[j, 5], rge[j, 12]), rnorm(1, rge[j, 7], 
+                  rge[j, 14]), rnorm(1, rge[j, 8], rge[j, 15]))
+        names(rvec) <- species6
         a.Y <- matrix(1, 1, 6) %*% invV %*% rvec/sum(invV)
         a.X <- matrix(1, 1, 6) %*% invV %*% eco/sum(invV)
-        rvec.gls <- (rvec - c(a.Y)) %*% invV %*% (eco - c(a.X))/sqrt(((rvec - c(a.Y)) %*% invV %*% (rvec - 
+        rvec.gls <- (rvec - c(a.Y)) %*% invV %*% 
+          (eco - c(a.X))/sqrt(((rvec - c(a.Y)) %*% invV %*% (rvec - 
             c(a.Y))) * ((eco - c(a.X)) %*% invV %*% (eco - c(a.X))))
         rset2[j] <- as.numeric(rvec.gls)
     }
@@ -143,19 +193,21 @@ phylr.mat <- matrix(NA,8,8)
 phylr.mat[upper.tri(phylr.mat)] <- phylrsummary[,1]
 phylr.mat[lower.tri(phylr.mat)] <- t(phylr.mat)[lower.tri(t(phylr.mat))]
 
+# Resample evolutionary correlation between ecomorph and genetic variances
+# (table A4)
 
 rvarset <- list()
-
 for (i in 1:10000) {
   rset2 <- c()
   for (j in 1:8) {
-    vvec <- c(mycrisG[[i]][j,j], myeverG[[i]][j,j], mygrahG[[i]][j,j], mylineG[[i]][j,j], 
-              mysagrG[[i]][j,j], mysmarG[[i]][j,j])
-    names(vvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "sagrei", "smaragdinus")
+    vvec <- c(mycrisG[[i]][j,j], myeverG[[i]][j,j], mygrahG[[i]][j,j], 
+              mylineG[[i]][j,j], mysagrG[[i]][j,j], mysmarG[[i]][j,j])
+    names(vvec) <- species6
     a.Y <- matrix(1, 1, 6) %*% invV %*% vvec/sum(invV)
     a.X <- matrix(1, 1, 6) %*% invV %*% eco/sum(invV)
-    vvec.gls <- (vvec - c(a.Y)) %*% invV %*% (eco - c(a.X))/sqrt(((vvec - c(a.Y)) %*% invV %*% (vvec - 
-                                                    c(a.Y))) * ((eco - c(a.X)) %*% invV %*% (eco - c(a.X))))
+    vvec.gls <- (vvec - c(a.Y)) %*% invV %*% (eco - 
+                c(a.X))/sqrt(((vvec - c(a.Y)) %*% invV %*% (vvec - 
+                c(a.Y))) * ((eco - c(a.X)) %*% invV %*% (eco - c(a.X))))
     rset2[j] <- as.numeric(vvec.gls)
   }
   rvarset[[i]] <- rset2
@@ -163,52 +215,6 @@ for (i in 1:10000) {
 rm(vvec, vvec.gls, a.Y, a.X, rset2)
 
 rvarsummary <- summarize_reps(rvarset)
-
-
-
-# Phylogenetic signal
-
-
-Kset <- list()
-
-for (i in 1:10000) {
-  Kset2 <- c()
-  for (j in 1:28) {
-    rvec <- c(rnorm(1, rge[j, 2], rge[j, 9]), rnorm(1, rge[j, 3], rge[j, 10]), rnorm(1, rge[j, 4], rge[j, 11]), 
-              rnorm(1, rge[j, 5], rge[j, 12]), rnorm(1, rge[j, 6], rge[j, 13]), rnorm(1, rge[j, 7], rge[j, 14]), 
-              rnorm(1, rge[j, 8], rge[j, 15]))
-    names(rvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", "sagrei", "smaragdinus")
-    Kset2[j] <- phylosig(anoletree, rvec)
-  }
-  Kset[[i]] <- Kset2
-}
-rm(rvec, Kset2)
-
-
-Ksummary <- summarize_reps(Kset)
-
-K.mat <- matrix(NA,8,8) 
-K.mat[upper.tri(K.mat)] <- Ksummary[,1]
-K.mat[lower.tri(K.mat)] <- t(K.mat)[lower.tri(t(K.mat))]
-
-
-
-Kvarset <- list()
-
-for (i in 1:10000) {
-  Kset2 <- c ()
-  for (j in 1:8) {
-    vvec <- c(mycrisG[[i]][j,j], myeverG[[i]][j,j], mygrahG[[i]][j,j], mylineG[[i]][j,j], mypulcG[[i]][j,j],
-              mysagrG[[i]][j,j], mysmarG[[i]][j,j])
-    names(vvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", "sagrei", "smaragdinus")
-    Kset2[j] <- phylosig(anoletree, vvec)
-  }
-  Kvarset[[i]] <- Kset2
-}
-rm(vvec, Kset2)
-  
-Kvarsummary <- summarize_reps(Kvarset)
-
 
 
 
--- Analyses without resampling.R
+++ Analyses without resampling.R
@@ -1,11 +1,12 @@
-#Load required packages
+# Load required packages
 
 library(ape)
 library(evolqg)
 library(mgcv)
+library(geomorph)
+library(phytools)
 
-
-#Load G matrices
+# Load G matrices (from McGlothlin et al. 2018)
 
 crisG<-as.matrix(read.table("data/cris.txt"))
 everG<-as.matrix(read.table("data/ever.txt"))
@@ -15,89 +16,114 @@ pulcG<-as.matrix(read.table("data/pulc.txt"))
 sagrG<-as.matrix(read.table("data/sagr.txt"))
 smarG<-as.matrix(read.table("data/smar.txt"))
 
+# Calculation of random skewers correlations (rRS, figs. 2-3, table A2 cols 1-3)
 
-
-#Random Skewers
-
-Gs<-list(crisG,everG,grahG,lineG,pulcG,sagrG,smarG)
-rsG<-RandomSkewers(Gs,num.vectors=10000)
+Gs<-list(crisG, everG, grahG, lineG, pulcG, sagrG, smarG)
+rsG<-RandomSkewers(Gs, num.vectors=10000)
 rsG
 
-#Correlation between genetic distance and random skewer correlations
+# Read trees and data for phylogenetic analyses. Trees as newick format, 
+# data as csv with one line per species, listing element and SE (from ASReml) 
+# separately
 
-geneticdist <- c(0.64127367, 0.987323604, 0.987323604, 0.541900856, 0.987323604, 1, 0.987323604, 0.987323604, 0.64127367, 
-                 0.987323604, 1, 0.744421662, 0.987323604, 0.894946327, 1, 0.987323604, 0.894946327, 1, 0.987323604, 1, 1)
-skews <- rsG$correlations[lower.tri(rsG$correlations, diag = FALSE)]
-cor(geneticdist, skews)
-
-#Partial correlation with ecomorph, controlling for genetic distance
-
-geneticdist6 <- c(0.64127367, 0.987323604, 0.987323604, 0.987323604, 1, 0.987323604, 0.987323604, 0.987323604, 1, 0.744421662, 
-                  0.89496327, 1, 0.987323604, 1, 1)
-
-eco.mat <- c(0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0)
-
-skews6 <- rsG$correlations[c(1:4, 6:7), c(1:4, 6:7)]
-skews6v <- skews6[lower.tri(skews6, diag = FALSE)]
-pcordat <- as.data.frame(cbind(eco.mat, geneticdist6, skews6v))
-pcor(pcordat)$estimate[1, 3]
-
-
-#Read trees and data for phylogenetic analyses. Trees as newick format, data as csv with one line per species, listing element and SE (from ASReml) separately
-
-rge<-as.data.frame(read.csv("rge.csv"))
-a6tree<-read.tree("pruned3.tre")
-anoletree<-read.tree("pruned7.tre")
+rge<-as.data.frame(read.csv("data/rge.csv"))
+a6tree<-read.tree("data/pruned3.tre")
+anoletree<-read.tree("data/pruned7.tre")
 anole.data<-as.data.frame(read.table("data/anoldat.txt"))
 eco <- as.integer(anole.data$ecomorph)
 names(eco) <- rownames(anole.data)
 
-#Construct phylogenetic covariance matrix
-V<-corMatrix(Initialize(corBrownian(phy=a6tree),anole.data))
+# Rough plot of genetic distance and rRS (fig. 3 plotted in GraphPad Prism)
 
+geneticdist <- 41.454817 * c(0.64127367, 0.987323604, 0.987323604, 0.541900856, 
+                             0.987323604, 1, 0.987323604, 0.987323604, 
+                             0.64127367, 0.987323604, 1, 0.744421662, 
+                             0.987323604, 0.894946327, 1, 0.987323604, 
+                             0.894946327, 1, 0.987323604, 1, 1)
+skews <- rsG$correlations[lower.tri(rsG$correlations, diag = FALSE)]
+plot(geneticdist, skews)
 
-#Phylogenetic correlations
+# K_mult for rRS (text)
 
-phylr<-c()
-for(j in 1:28){
-  rvec<-c(as.numeric(rge[j,2]),as.numeric(rge[j,3]),as.numeric(rge[j,4]),as.numeric(rge[j,5]),as.numeric(rge[j,7]),as.numeric(rge[j,8]))
-  names(rvec)<-c("cristatellus","evermanni","grahami","lineatopus","sagrei","smaragdinus")
-  a.Y <- matrix(1,1,6) %*% solve(V) %*% rvec/sum(solve(V));
-  a.X <- matrix(1,1,6) %*% solve(V) %*% eco/sum(solve(V));
-  rvec.gls<-(rvec-c(a.Y)) %*% solve(V) %*% (eco-c(a.X))/sqrt(((rvec-c(a.Y)) %*% solve(V) %*% (rvec-c(a.Y)))*((eco-c(a.X)) %*% solve(V) %*% (eco-c(a.X))));
-  phylr[j]<-as.numeric(rvec.gls)
+rsmatpcoa <- function(x, sp.names){
+  skmat <-  matrix(rep(1,length(x)), nrow(x), ncol(x))
+  skmat[lower.tri(skmat, diag = FALSE)] <- x[lower.tri(x, diag=FALSE)]
+  skmat[upper.tri(skmat, diag = FALSE)] <- t(skmat)[upper.tri(skmat, 
+                                                              diag = FALSE)]
+  row.names(skmat) <- sp.names
+  colnames(skmat) <- sp.names
+  skdist <- sqrt(2*(1-skmat))
+  pcoa(skdist)$vectors
 }
 
-phylovar <- c()
-for (j in 1:8) {
-  vvec <- c(crisG[j,j], everG[j,j], grahG[j,j], lineG[j,j], 
-            sagrG[j,j], smarG[j,j])
-  names(vvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "sagrei", "smaragdinus")
-  a.Y <- matrix(1, 1, 6) %*% invV %*% vvec/sum(invV)
-  a.X <- matrix(1, 1, 6) %*% invV %*% eco/sum(invV)
-  vvec.gls <- (vvec - c(a.Y)) %*% invV %*% (eco - c(a.X))/sqrt(((vvec - c(a.Y)) %*% invV %*% (vvec - 
-                                                                                                c(a.Y))) * ((eco - c(a.X)) %*% invV %*% (eco - c(a.X))))
-  phylovar[j] <- as.numeric(vvec.gls)
-}
+species <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", 
+             "sagrei", "smaragdinus")
 
+physignal(rsmatpcoa(rsG$correlations, species), anoletree)
 
+# Mantel with phylogenetically informed permuations for ecomorph effect (text)
 
-#Phylogenetic signal
+species6 <- c("cristatellus", "evermanni", "grahami", "lineatopus", "sagrei", 
+              "smaragdinus")
+eco.mat <- c(0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0)
+eco6mat <- matrix(rep(0,36), 6, 6)
+eco6mat[lower.tri(eco6mat, diag = FALSE)] <- 1 - eco.mat
+eco6mat[upper.tri(eco6mat, diag = FALSE)] <- t(eco6mat)[upper.tri(eco6mat, 
+                                                                  diag = FALSE)]
+colnames(eco6mat) <- species6
+row.names(eco6mat) <- species6
+
+PhyloMantel(a6tree, rs6dist, eco6mat)
+
+# Construct phylogenetic covariance matrix for individual element analyses
+
+V <- corMatrix(Initialize(corBrownian(phy=a6tree), anole.data))
+
+# Phylogenetic signal (table A3)
 
 Kr <- c()  
 for (j in 1:28) {
-  rvec<-c(as.numeric(rge[j,2]),as.numeric(rge[j,3]),as.numeric(rge[j,4]),as.numeric(rge[j,5]),as.numeric(rge[j,6]),as.numeric(rge[j,7]),as.numeric(rge[j,8]))
-  names(rvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", "sagrei", "smaragdinus")
+  rvec<-c(as.numeric(rge[j,2]), as.numeric(rge[j,3]), as.numeric(rge[j,4]),
+          as.numeric(rge[j,5]), as.numeric(rge[j,6]), as.numeric(rge[j,7]),
+          as.numeric(rge[j,8]))
+  names(rvec) <- species
   Kr[j] <- phylosig(anoletree, rvec)
 }
-
-
 
 Kvar <- c()
 for (j in 1:8) {
   vvec <- c(crisG[j,j], everG[j,j], grahG[j,j], lineG[j,j], pulcG[j,j],
             sagrG[j,j], smarG[j,j])
-  names(vvec) <- c("cristatellus", "evermanni", "grahami", "lineatopus", "pulchellus", "sagrei", "smaragdinus")
+  names(vvec) <- species
   Kvar[j] <- phylosig(anoletree, vvec)
 }
+
+
+# Evolutionary correlations (fig. 4, table A4)
+
+phylovar <- c()
+for (j in 1:8) {
+  vvec <- c(crisG[j,j], everG[j,j], grahG[j,j], lineG[j,j], 
+            sagrG[j,j], smarG[j,j])
+  names(vvec) <- species6
+  a.Y <- matrix(1, 1, 6) %*% invV %*% vvec/sum(invV)
+  a.X <- matrix(1, 1, 6) %*% invV %*% eco/sum(invV)
+  vvec.gls <- (vvec - c(a.Y)) %*% invV %*% 
+    (eco - c(a.X))/sqrt(((vvec - c(a.Y)) %*% invV %*% (vvec - c(a.Y))) * 
+                          ((eco - c(a.X)) %*% invV %*% (eco - c(a.X))))
+  phylovar[j] <- as.numeric(vvec.gls)
+}
+
+phylr <- c()
+for(j in 1:28){
+  rvec <- c(as.numeric(rge[j,2]),as.numeric(rge[j,3]),as.numeric(rge[j,4]),
+            as.numeric(rge[j,5]),as.numeric(rge[j,7]),as.numeric(rge[j,8]))
+  names(rvec) <- species6
+  a.Y <- matrix(1,1,6) %*% solve(V) %*% rvec/sum(solve(V));
+  a.X <- matrix(1,1,6) %*% solve(V) %*% eco/sum(solve(V));
+  rvec.gls<-(rvec-c(a.Y)) %*% solve(V) %*% (eco-c(a.X))/sqrt(((rvec-c(a.Y)) %*% 
+      solve(V) %*% (rvec-c(a.Y)))*((eco-c(a.X)) %*% solve(V) %*% (eco-c(a.X))));
+  phylr[j]<-as.numeric(rvec.gls)
+}
+
 
